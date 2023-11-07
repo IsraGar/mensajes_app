d60230728789e431ebc974aba19b931d4144fa4b
Israel Garcia <isragarciahdez@gmail.com>
2023-11-07 14:37:38 -0600

    Adding insert method

--- src/main/java/MensajeService.java
+++ src/main/java/MensajeService.java
@@ -1,6 +1,21 @@
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Scanner;
+
 public class MensajeService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+        System.out.println("Tu nombre");
+        String nombre = sc.nextLine();
+        Timestamp fecha = new Timestamp(System.currentTimeMillis());
 
+        Mensaje registro = new Mensaje();
+        registro.setMensaje(mensaje);
+        registro.setAutorMensaje(nombre);
+        registro.setFechaMensaje(fecha.toString());
+        MensajesDAO.crearMensajeDB(registro);
     }
     public static void listarMensaje(){
 
--- src/main/java/MensajesDAO.java
+++ src/main/java/MensajesDAO.java
@@ -1,6 +1,26 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void crearMensajeDB(Mensaje mensaje){
-
+        Conexion dbConnect = new Conexion();
+        try(Connection connection = dbConnect.GetConnection()){
+            PreparedStatement preparedStatement = null;
+            try{
+                String query = "INSERT INTO mensajes (mensaje, autor_mensaje, fecha_mensaje) VALUES (?,?,?)";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, mensaje.getMensaje());
+                preparedStatement.setString(2, mensaje.getAutorMensaje());
+                preparedStatement.setString(3, mensaje.getFechaMensaje());
+                preparedStatement.executeUpdate();
+                System.out.println("Mensaje creado de manera correcta");
+            }catch (SQLException ex){
+                System.out.println(ex);
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
     public static void leerMensajesDB(){
 
